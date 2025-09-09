93dbdda7ce4af098ec4ba2980c86ae3f34949c0e
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 16:00:19 -0400

    adding implementation for mergeandsort that copies the length, sorts and returns the merged array.

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -16,6 +16,13 @@ public class MergeSortedArray {
         System.out.println("Merged and Sorted Array: " + Arrays.toString(mergedArray));
         System.out.println("Middle Value: " + middleValue);
     }
+    private static int[] mergeAndSort(int[] array1, int[] array2) {
+        int[] mergedArray = new int[array1.length + array2.length];
+        System.arraycopy(array1, 0, mergedArray, 0, array1.length);
+        System.arraycopy(array2, 0, mergedArray, array1.length, array2.length);
+        Arrays.sort(mergedArray);
+        return mergedArray;
+    }
 
 
 
