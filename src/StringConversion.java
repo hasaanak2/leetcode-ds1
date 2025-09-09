1ff03afdc92e67e0b45829d3dc36ca9f1f52abe8
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 19:04:39 -0400

    loop thru each character and creating logic to apend thru the first and second halves of string

--- src/StringConversion.java
+++ src/StringConversion.java
@@ -7,6 +7,18 @@ public class StringConversion {
 
         int len = str.length();
 
+        // Loop through each character in the input string
+        for (int i = 0; i < len; i++) {
+            // Check if the current character is in the first half of the string
+            if (i <= len / 2) {
+                // Convert the character to uppercase and append it to the result
+                char a = Character.toUpperCase(str.charAt(i));
+                strBldr.append(a);
+            } else {
+                // if the character is in the second half, just append it as is
+                strBldr.append(str.charAt(i));
+            }
+        }
 
         // Print the final result
         System.out.println(strBldr);
