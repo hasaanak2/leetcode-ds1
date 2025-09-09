aafcdeab1366c8ddaec0b3144fb380fa58434e47
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 19:01:35 -0400

    creating input string, stringbuilder, defining length and priting out a result.

--- src/StringConversion.java
+++ src/StringConversion.java
@@ -1,3 +1,14 @@
 public class StringConversion {
+    public static void main(String[] args) {
+        // Input string
+        String str = "thisisastringconversionchallenge!";
 
+        StringBuilder strBldr = new StringBuilder();
+
+        int len = str.length();
+
+
+        // Print the final result
+        System.out.println(strBldr);
+    }
 }
