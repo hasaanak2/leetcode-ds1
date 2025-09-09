d6c655e01eee42fd7149eba534892418063171b6
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 15:54:04 -0400

    added main function to print and test.

--- src/AnagramCheck.java
+++ src/AnagramCheck.java
@@ -4,6 +4,14 @@
 * code and implement a solution
  */
 
+import java.util.Arrays;
+
 public class AnagramCheck {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        System.out.println("Are the two strings anagrams? ");
+    }
 
 }
