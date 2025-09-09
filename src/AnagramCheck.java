0c00e487f37078e7aa9af6271b8731c854af317f
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 15:55:48 -0400

    adding implementation logic for anagrams check.

--- src/AnagramCheck.java
+++ src/AnagramCheck.java
@@ -11,7 +11,19 @@ public class AnagramCheck {
         String str1 = "listen";
         String str2 = "silent";
 
-        System.out.println("Are the two strings anagrams? ");
+        boolean isAnagram = areAnagrams(str1, str2);
+        System.out.println("Are the two strings anagrams? " + isAnagram);
+    }
+
+    private static boolean areAnagrams(String str1, String str2) {
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
     }
 
 }
