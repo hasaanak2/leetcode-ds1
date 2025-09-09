25dc3a94f50214b7cbd89734e0f12146fd680681
Hasaan Keen <Hasaankeen1@gmail.com>
2025-09-09 18:25:25 -0400

    initialized the variables and a print out statement to return.

--- src/FibonacciSequence.java
+++ src/FibonacciSequence.java
@@ -4,4 +4,11 @@
  */
 
 public class FibonacciSequence {
+
+    public static void main(String[] args) {
+        double a = 0.0;
+        double b = 1.0;
+
+        System.out.print(a + ", " + b + ", ");
+    }
 }
